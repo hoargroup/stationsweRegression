538b9f06c98ea652e7d5a9465703ee8516bb8f2e
Dominik Schneider <dominik.schneider@colorado.edu>
2017-06-02 14:28:38 +0200

    new function to format output in tsv without scientific notation

---
+++ R/format_numeric.R
@@ -0,0 +1,9 @@
+#' helper function to format output to csvs
+#' @param x dataframe to format
+#' @details base::format does not appear to produce a data.frame so it can't be used with write_tsv. I typically use this with sci=FALSE to turn off scientific notation but it can also control significant digits, decimal places etc. see `format` for options.
+
+format_numeric <- function(x, ...) {
+	numeric_cols <- vapply(x, is.numeric, logical(1))
+	x[numeric_cols] <- lapply(x[numeric_cols], format, ...)
+	x
+}
